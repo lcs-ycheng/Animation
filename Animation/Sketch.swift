ab3259fdce063a3f1ddb7f14b9d5e84df2558c4d
ycheng <ycheng@lcs-mba3103.lcs.on.ca>
2017-10-25 10:27:38 -0400

    The circle bangs by left and right sides

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    //change in position
+    var dx : Int //difference in x
+    
     // This function runs once
     override init() {
         
@@ -16,17 +19,35 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
         
+        //set the difference for x
+        dx = 3
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        //clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
         // Change position
-        x += 1
+        x += dx
+        
+        //check the position and reverce course
+        //if we go off the right edge of the screem
+        if x > 500{
+            dx = -3
+        }
+        
+        //if we go off the left edge of the screem
+        if x < 0 {
+            dx = 3
+        }
         
         // Draw an ellipse in the middle of the canvas
+        canvas.fillColor = Color.black
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
