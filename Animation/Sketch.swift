636a0df33a48495a841725caea5a59f27400587f
ycheng <ycheng@lcs-mba3103.lcs.on.ca>
2017-10-26 20:12:35 -0400

    finish the four corners

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -26,8 +26,20 @@ class Sketch : NSObject {
         // Change position
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Draw an ellipse and change the color of the ellipse in the middle of the canvas
+        canvas.fillColor = Color.red
+        canvas.borderColor = Color.red
+        canvas.drawEllipse(centreX: x, centreY: x, width: 50, height: 50)
+        canvas.fillColor = Color.purple
+        canvas.borderColor = Color.purple
+        canvas.drawEllipse(centreX: x, centreY: 500-x, width: 50, height: 50)
+        canvas.fillColor = Color.yellow
+        canvas.borderColor = Color.yellow
+        canvas.drawEllipse(centreX: 500-x, centreY: x, width: 50, height: 50)
+        canvas.fillColor = Color.green
+        canvas.borderColor = Color.green
+        canvas.drawEllipse(centreX: 500-x, centreY: 500-x, width: 50, height: 50)
+      
         
     }
     
